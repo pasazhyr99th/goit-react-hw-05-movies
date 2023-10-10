3945442e1006183cce227250336a3e643b00cec3
pasazhyr99th <108014339+pasazhyr99th@users.noreply.github.com>
2023-10-10 22:18:42 +0000

    Deploying to gh-pages from @ pasazhyr99th/goit-react-hw-05-movies@c95674ce708f23e16b92d633fe26b276ef4e1e6a 🚀

---
+++ static/js/266.216f18d0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("div",{children:"Trending today"})}}}]);
+//# sourceMappingURL=266.216f18d0.chunk.js.map
